03a6a55b0d71cb070d3690f18ed1c0a5a36e551c
Dipak <dipak@htree.plus>
2022-09-30 16:56:10 +0530

    add question intents o 30th sep

--- actions/actions.py
+++ actions/actions.py
@@ -7,6 +7,13 @@ from rasa.core.nlg import NaturalLanguageGenerator as nlg
 from sentence_transformers import SentenceTransformer, util
 import pandas as pd
 import json
+import nltk
+from nltk import tokenize
+from operator import itemgetter
+import math
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
 data=pd.read_json('/home/fx/Dev/AI_chatbot/Excel_File/rightmove.json').T
 
@@ -21,6 +28,58 @@ class Information(Action):
         chosen_property = tracker.get_slot('chosen_property')
         self.property_detail = data.iloc[int(chosen_property)]
         return self.property_detail
+
+    def search_query(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any], q, context) -> List[Dict[Text, Any]]:
+        model_name = "deepset/roberta-base-squad2"
+
+        nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+        QA_input = {
+            'question': f'{q}',
+            'context': f"""{context}"""
+        }
+        res = nlp(QA_input)
+        return [res]
+
+    def extract_keywords(doc):
+        def check_sent(word, sentences): 
+            final = [all([w in x for w in word]) for x in sentences] 
+            sent_len = [sentences[i] for i in range(0, len(final)) if final[i]]
+            return int(len(sent_len))
+        def get_top_n(dict_elem, n):
+            result = dict(sorted(dict_elem.items(), key = itemgetter(1), reverse = True)[:n]) 
+            return result
+        stop_words = set(stopwords.words('english'))
+        total_sentences = tokenize.sent_tokenize(doc)
+        total_sent_len = len(total_sentences)
+        total_words = doc.split()
+        total_word_length = len(total_words)
+        tf_score = {}
+        for each_word in total_words:
+            each_word = each_word.replace('.','')
+            if each_word not in stop_words:
+                if each_word in tf_score:
+                    tf_score[each_word] += 1
+                else:
+                    tf_score[each_word] = 1
+
+        # Dividing by total_word_length for each dictionary element
+        tf_score.update((x, y/int(total_word_length)) for x, y in tf_score.items())
+        idf_score = {}
+        for each_word in total_words:
+            each_word = each_word.replace('.','')
+            if each_word not in stop_words:
+                if each_word in idf_score:
+                    idf_score[each_word] = check_sent(each_word, total_sentences)
+                else:
+                    idf_score[each_word] = 1
+
+        # Performing a log and divide
+        idf_score.update((x, math.log(int(total_sent_len)/y)) for x, y in idf_score.items())
+        tf_idf_score = {key: tf_score[key] * idf_score.get(key, 0) for key in tf_score.keys()}
+        keywords_dict = get_top_n(tf_idf_score, 5)
+        keywords = keywords_dict.keys()
+
+        return list(keywords)
 
 
 
@@ -239,6 +298,43 @@ class CheckBathroomInfo(Information):
         elif "no" in self.property_detail['Bathroom'].iloc[0].lower():
             dispatcher.utter_message(text=f"no, this property does not includes bathroom.")
         return []
+
+
+
+class CheckFeaturesInfo(Information):
+    def __init__(self):
+        super().__init__()
+
+    def name(self) -> Text:
+        return "action_check_features"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return []
+
+class CheckFeaturesInfo(Information):
+    def __init__(self):
+        super().__init__()
+
+    def name(self) -> Text:
+        return "action_check_property_rent_or_sale"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return []
+
+
+class CheckLiftInfo(Information):
+    def __init__(self):
+        super().__init__()
+
+    def name(self) -> Text:
+        
+        return "action_check_lift"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        super().run(dispatcher, tracker, domain)
+        return []
+
+
 
 
 # class CheckHouseInfo(Information):
